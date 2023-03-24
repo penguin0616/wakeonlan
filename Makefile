cfe8dfb51c1992f722d05ebbd9f6012ea737286f
penguin0616 <23770056+penguin0616@users.noreply.github.com>
2023-03-23 20:10:23 -0500

    first commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+CC=gcc
+
+domake: 
+	$(CC) wakeonlan.c -o wakeonlan -std=c99
---
+++ wakeonlan.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+void createMagicPacket(char mac_address) {
+	char payload[106];
+	
+	memset(&payload, 0, sizeof(payload));
+}
+
+int main() {
+	printf("Hello, world!\n");
+	return 0;
+}
